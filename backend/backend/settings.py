27720473c07c853073693661192a2a666eaf4c69
Ameesha M A <119642723+myself-ameesha@users.noreply.github.com>
2025-08-03 09:26:45 +0530

    settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,9 +33,14 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    "gymnet.shop",
+    "api.gymnet.shop",
+    "localhost",
+    "127.0.0.1",
+]
 
 AUTH_USER_MODEL = 'api.User' 
 
